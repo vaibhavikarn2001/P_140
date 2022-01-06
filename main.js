b79e8cd15c03cf4874f6e1f746e50b454710314d
Vaibhavi Karn <76275888+vaibhavikarn2001@users.noreply.github.com>
2022-01-06 20:11:53 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,99 @@
+var paddle2 =10,paddle1=10;
+
+var paddle1X = 10,paddle1Height = 110;
+var paddle2Y = 685,paddle2Height = 70;
+
+var score1 = 0, score2 =0;
+var paddle1Y;
+
+var  playerscore =0;
+
+var pcscore =0;
+//ball x and y and speedx speed y and radius
+var ball = {
+    x:350/2,
+    y:480/2,
+    r:20,
+    dx:3,
+    dy:3
+}
+
+rightWristY = 0;
+rightWristX = 0;
+scoreRightWrist = 0;
+
+game_status = "";
+
+ function preload() {
+  ball_touch_paddel = loadSound("ball_touch_paddel.wav");
+  missed = loadSound("missed.wav");
+}
+
+function setup(){
+var canvas =  createCanvas(700,600);
+canvas.parent('canvas');
+
+video = createCapture(VIDEO);
+video.size(700, 600);
+video.hide();
+
+poseNet = ml5.poseNet(video, modelLoaded);
+poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded() {
+  console.log('PoseNet Is Initialized');
+}
+
+function gotPoses(results)
+{
+  if(results.length > 0)
+  {
+
+    rightWristY = results[0].pose.rightWrist.y;
+    rightWristX = results[0].pose.rightWrist.x;
+    scoreRightWrist =  results[0].pose.keypoints[10].score;
+    console.log(scoreRightWrist);
+  }
+}
+function draw(){
+	image(video,0,0,480,480);
+}
+
+
+//function midline draw a line in center
+
+
+//very important function of this game
+
+
+
+//width height of canvas speed of ball 
+function models(){
+    textSize(18);
+    fill(255);
+    noStroke();
+    text("Width:"+width,135,15);
+    text("Speed:"+abs(ball.dx),50,15);
+    text("Height:"+height,235,15)
+}
+
+
+//this function help to not go te paddle out of canvas
+function paddleInCanvas(){
+  if(mouseY+paddle1Height > height){
+    mouseY=height-paddle1Height;
+  }
+  if(mouseY < 0){
+    mouseY =0;
+  }
+ 
+  
+}
+
+function restart()
+{
+  loop();
+  pcscore = 0;
+  playerscore = 0;
+}
